26c3b93f5c399ea1cd14c6fd1d94d17604141436
HeRoMo <hero@hero-mac.local>
2016-11-15 00:44:10 +0900

    add like and unlike button

--- app/assets/javascripts/lists.js
+++ app/assets/javascripts/lists.js
@@ -1,2 +1,15 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(function(){
+  $("#like").on('ajax:success', function(event, data, status, xhr){
+    $(event.target).addClass('yes');
+    $('#unlike').addClass('yes')
+  })
+  $('#unlike').on('ajax:success', function(event, data, status, xhr){
+    $(event.target).removeClass('yes');
+    $('#like').removeClass('yes')
+  })
+});
+
+

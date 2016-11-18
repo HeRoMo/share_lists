8041147b2d33a0964741bbd3d51fcf22c23cf269
HeRoMo <hero@hero-mac.local>
2016-11-19 00:56:51 +0900

    add seeds data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,24 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Admin.create({email: 'admin@email.com', password: 'password', password_confirmation:'password'})
-User.create({email: 'first@email.com', password: 'password', password_confirmation:'password'})
+admin = Admin.create({email: 'admin@email.com', password: 'password', password_confirmation:'password'})
+first = User.create({email: 'first@email.com', password: 'password', password_confirmation:'password'})
 
 20.times do |i|
   List.create({
       title: "title-#{i}",
       items: "item-#{i}.1\nitem-#{i}.2\nitem-#{i}.3",
       owner: User.first
+              })
+end
+
+require 'csv'
+CSV.foreach("#{File.dirname(__FILE__)}/list_seeds.csv",headers: true) do |row|
+  List.create({
+      title: row['title'],
+      description: row['description'],
+      items: row['items'],
+      memo:row['memo'],
+      owner:first
               })
 end
